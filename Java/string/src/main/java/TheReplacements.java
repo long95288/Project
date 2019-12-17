283856ed39e463cf3707327a82843d4e1b0577e5
longquanxiao <1186481819@qq.com>
2019-12-17 19:54:43 +0800

    摸鱼

--- Java/string/src/main/java/TheReplacements.java
+++ Java/string/src/main/java/TheReplacements.java
@@ -4,6 +4,12 @@
  */
 public class TheReplacements {
     public static void main(String[] args) throws Exception{
+        String str = "a b c d e f g a b c d e f g";
 
+        String str1 = str.replaceAll("a","a1");
+        System.out.println(str1);
+
+        String str2 = str.replaceFirst("b","b1");
+        System.out.println(str2);
     }
 }
