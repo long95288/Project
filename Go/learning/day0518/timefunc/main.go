2da9095de816abee69b2e30ed0ddcd9d98e31107
longquanxiao <1186481819@qq.com>
2020-05-18 20:41:24 +0800

    time

--- Go/learning/day0518/timefunc/main.go
+++ Go/learning/day0518/timefunc/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "strconv"
     "time"
 )
 
@@ -17,6 +18,38 @@ func main() {
     fmt.Println("min = ",now.Minute())
     fmt.Println("second = ",now.Second())
     
+    fmt.Printf("当前年月日:%d %d %d %d:%d:%d\n",
+        now.Year(),now.Month(),now.Day(),now.Hour(),now.Minute(),now.Second())
     
+    datestr := fmt.Sprintf("当前年月日:%d %d %d %d:%d:%d",
+        now.Year(),now.Month(),now.Day(),now.Hour(),now.Minute(),now.Second())
+    fmt.Println(datestr)
+    fmt.Println(now.Format("2006-01-02 15:04:05"))
+    now = time.Now()
+    fmt.Println(now.Format("2006-01-02 03:04:05"))
+    fmt.Printf("unix = %v unixnano = %v\n",now.Unix(),now.UnixNano())
+    
+    start := time.Now().Unix()
+    strf := func() func()  {
+        return func() {
+            str := ""
+            for i := 0; i < 100000; i++ {
+                str += "Hello" + strconv.Itoa(i)
+            }
+        }
+        }()
+    strf()
+    end := time.Now().Unix()
+    fmt.Printf("执行%v秒\n",end -start)
+    
+    i := 0
+    for {
+        i++
+        fmt.Println(i)
+        time.Sleep(time.Millisecond*100)
+        if i == 100 {
+            break
+        }
+    }
     
 }
