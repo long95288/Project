4507f8707f9000188c2801ca5c819227ae3b1383
longquanxiao <1186481819@qq.com>
2020-06-24 00:02:27 +0800

    comment

--- day0623/demo01/server/rpcServer.go
+++ day0623/demo01/server/rpcServer.go
@@ -14,15 +14,18 @@ type Quotient struct{
     Quo, Rem int
 }
 
+// rpc 的结构对象
 type Arith int
-
 /*
-两数之和
+两数相乘函数，被调用
 */
 func (t *Arith) Multiply(args *Args, reply *int) error{
     *reply = args.A + args.B
     return nil
 }
+/*
+两数相除函数，被调用
+*/
 func (t *Arith) Divide(args *Args, quo *Quotient) error{
     if args.B == 0{
         return errors.New("Divide by zero")
