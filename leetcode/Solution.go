155acfac7b9b13539c4384c15697b1ae960b9b80
longquanxiao <1186481819@qq.com>
2020-08-06 21:39:33 +0800

    update leetcode

--- leetcode/Solution.go
+++ leetcode/Solution.go
@@ -21,3 +21,24 @@ func hasCycle(head *ListNode) bool {
     }
     return false
 }
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+    m := make(map[*ListNode]interface{})
+    if headA == nil || headB == nil {
+        return nil
+    }
+    cur := headA
+    for cur != nil {
+        m[cur] = true
+        cur = cur.Next
+    }
+    // 遍历B的时候,判断
+    cur = headB
+    for cur != nil {
+        if _,ok := m[cur];ok{
+            return cur
+        }
+        cur = cur.Next
+    }
+    return nil
+}
