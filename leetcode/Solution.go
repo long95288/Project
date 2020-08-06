9003f9bd46e9fb1885bd0e5b657b59135621343d
longquanxiao <1186481819@qq.com>
2020-08-06 21:00:15 +0800

    update leetcode

---
+++ leetcode/Solution.go
@@ -0,0 +1,23 @@
+package leetcode
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+
+/**
+
+ */
+func hasCycle(head *ListNode) bool {
+    cur := head
+    m := make(map[*ListNode]interface{})
+    for cur != nil{
+        if _,ok := m[cur];ok {
+            return true
+        }
+        m[cur] = nil
+        cur = cur.Next
+    }
+    return false
+}
