365352597af65286c8b14b79a035aa1fedd177b9
longquanxiao <1186481819@qq.com>
2020-08-16 23:34:11 +0800

    feat:实现远端控制笔记本电脑关机脚本

---
+++ remoteController/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "fmt"
+    "github.com/gin-gonic/gin"
+    "log"
+    "net/http"
+    "os/exec"
+)
+
+func main() {
+    e:= gin.Default()
+    e.GET("/cmd",HandleController)
+    err := e.Run(":9999")
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Println("service start at http://localhost:9999")
+}
+
+func shutdown(c *gin.Context){
+    args :=[]string{"-s","-t","30"}
+    cmd := exec.Command("shutdown",args...)
+    err := cmd.Run()
+    if err != nil {
+        c.JSON(http.StatusInternalServerError,gin.H{
+            "message":fmt.Sprintf("关机失败,err : %v",err),
+        })
+    }else{
+        c.JSON(http.StatusOK,gin.H{
+            "message": "电脑将在30s后关机",
+        })
+    }
+}
+func cancelShutdown(c *gin.Context){
+    args := []string{"-a"}
+    cmd := exec.Command("shutdown",args...)
+    err := cmd.Run()
+    if err != nil {
+        c.JSON(http.StatusInternalServerError,gin.H{
+            "message":fmt.Sprintf("取消关机失败,err : %v",err),
+        })
+    }else{
+        c.JSON(http.StatusOK,gin.H{
+            "message":"取消关机成功",
+        })
+    }
+}
+
+func HandleController(c *gin.Context)  {
+    id := c.Query("id")
+    switch id {
+    case "1":
+        shutdown(c)
+    case "2":
+        cancelShutdown(c)
+    default:
+        c.JSON(
+            http.StatusBadRequest,
+            gin.H{
+                "message":"bad Request",
+        })
+    }
+}
