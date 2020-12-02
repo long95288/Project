b62780ebb3c55cf694c82206932ebfc54f68d814
longquanxiao <1186481819@qq.com>
2020-12-02 20:38:27 +0800

    update:leetcode

--- leetcode/Solution.go
+++ leetcode/Solution.go
@@ -3579,3 +3579,44 @@ func mostCompetitive(nums []int, k int) []int {
     }
     return ans
 }
+type MaxQueue struct {
+    queue []int
+    maxQueue []int
+}
+
+
+func Constructor9() MaxQueue {
+    return MaxQueue{queue: []int{}, maxQueue: []int{}}
+}
+
+
+func (this *MaxQueue) Max_value() int {
+    if len(this.maxQueue) < 1 {
+        return -1
+    }
+    return this.maxQueue[0]
+}
+
+
+func (this *MaxQueue) Push_back(value int)  {
+    this.queue = append(this.queue, value)
+    // 最大值
+    for len(this.maxQueue) >= 0 && this.maxQueue[len(this.maxQueue) - 1] < value {
+        this.maxQueue = this.maxQueue[ : len(this.maxQueue) -1]
+    }
+    this.maxQueue = append(this.maxQueue, value)
+    this.queue = append(this.queue, value)
+}
+
+
+func (this *MaxQueue) Pop_front() int {
+    if len(this.queue) < 1 {
+        return - 1
+    }
+    ans := this.queue[0]
+    this.queue = this.queue[1:]
+    if ans == this.maxQueue[0] {
+        this.maxQueue = this.maxQueue[1:]
+    }
+    return ans
+}
