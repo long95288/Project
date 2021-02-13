3ba567f0d38edbdb0440bb795259e41bd40b283e
longquanxiao <1186481819@qq.com>
2021-02-13 20:47:26 +0800

    feat:App server

--- remoteController/main.go
+++ remoteController/main.go
@@ -5,6 +5,7 @@ import (
     "github.com/gin-gonic/gin"
     "html/template"
     "log"
+    "net"
     "net/http"
     "os/exec"
     "strconv"
@@ -16,8 +17,72 @@ type status struct {
 }
 var globalStatus = status{Status: "正常"}
 const TIME_LAYOUT = "2006-01-02T15:04"
+var exit_app_server = false
+
+/*
+* app连接线程
+*/
+func appConnHandler(conn net.Conn) {
+    if nil == conn {
+        log.Println("错误APP连接")
+        return
+    }
+    go func() {
+        for !exit_app_server {
+            readbuf := make([]byte, 4096)
+            n,err := conn.Read(readbuf)
+            if nil != err || n < 0 {
+                log.Println("读取数据失败")
+                break
+            }
+            fmt.Printf("App server read data size = %d\n", n)
+            fmt.Println("App server read data :" + string(readbuf[:n + 1]))
+            //for i := 0;i < n;i ++ {
+            //    if i > 0 && i % 16 ==0 {
+            //        fmt.Println("")
+            //    }
+            //    fmt.Printf("%x ", readbuf[i])
+            //}
+            time.Sleep(100*time.Millisecond)
+        }
+    }()
+    
+    go func() {
+        for !exit_app_server {
+            writebuf := []byte("SERVER SAY HELLO WORLD")
+            n, err := conn.Write(writebuf)
+            if nil != err || n != len(writebuf) {
+                fmt.Println("App Server write data failed.\n")
+                break
+            }
+            time.Sleep(100*time.Millisecond)
+        }
+    }()
+    
+}
+
+func App_Server()  {
+    server, err := net.Listen("tcp", ":1399")
+    if nil != err {
+        fmt.Println(err)
+    }
+    fmt.Println("开启APP服务器")
+    for !exit_app_server {
+        fmt.Println("等待连接....")
+        conn, err := server.Accept()
+        fmt.Println("接受连接....")
+        if nil != err{
+            fmt.Println(err)
+            
+        }else{
+            go appConnHandler(conn)
+        }
+        time.Sleep(200 * time.Millisecond)
+    }
+}
 
 func main() {
+    App_Server()
     e:= gin.Default()
     e.GET("/cmd",HandleController)
     e.GET("/", func(context *gin.Context) {
