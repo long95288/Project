0488092cc051206532ff5409f7fceea41e137c8b
longquanxiao <1186481819@qq.com>
2021-02-25 22:38:29 +0800

    添加获取和设置windows音量控制

---
+++ remoteController/cmd.go
@@ -0,0 +1,96 @@
+package main
+// 命令行,只做中间层,不负责HTTP相关操作
+import (
+    "fmt"
+    "github.com/gin-gonic/gin"
+    "html/template"
+    "log"
+    "net/http"
+    "os/exec"
+    "strconv"
+)
+const (
+    SETSHUTDOWNPLAN_CMD = 1
+    CANCELSHUTDOWNPLAN_CMD = 2
+    GETMASTERVOLUME_CMD = 3
+    SETMASTERVOLUME_CMD = 4
+)
+var WindowControllerCmd = "WindowsRemoteController.exe"
+func SetMasterVolume(volume float64) (float64, error) {
+    args := []string{"2"}
+    args =  append(args, fmt.Sprintf("%f", volume))
+    cmd := exec.Command(WindowControllerCmd, args...)
+    output, err := cmd.CombinedOutput()
+    if nil != err {
+        return 0, err
+    }
+    volume1, err := strconv.ParseFloat(string(output), 10)
+    return volume1, err
+}
+func GetMasterVolume() (float64, error) {
+    args := []string{"1"}
+    cmd := exec.Command(WindowControllerCmd, args...)
+    output, err := cmd.CombinedOutput()
+    if err != nil {
+        return 0, err
+    }
+    volume, err := strconv.ParseFloat(string(output), 10)
+    return volume, err
+}
+func SetShutdownPlan(time int) (string, error) {
+    args := []string{"-s", "-t"}
+    args = append(args,fmt.Sprintf("%d", time))
+    cmd := exec.Command("shutdown", args...)
+    output, err := cmd.CombinedOutput()
+    return string(output), err
+}
+func CancelShutdownPlan() (string, error) {
+    args := []string{"-a"}
+    cmd := exec.Command("shutdown", args...)
+    output, err := cmd.CombinedOutput()
+    return string(output), err
+}
+
+func shutdown2(c *gin.Context)  {
+    args :=[]string{"-s","-t","30"}
+    cmd := exec.Command("shutdown",args...)
+    err := cmd.Run()
+    if err != nil {
+        c.JSON(http.StatusInternalServerError,
+            gin.H{"message":"服务器内部错误"})
+    }else{
+        globalStatus.Status = "电脑将在30s后关机"
+        c.JSON(http.StatusOK, gin.H{"message":globalStatus.Status})
+    }
+}
+func shutdown(c *gin.Context){
+    args :=[]string{"-s","-t","30"}
+    t,err := template.ParseFiles("index.html")
+    if err != nil {
+        log.Println("err : ", err)
+        return
+    }
+    cmd := exec.Command("shutdown",args...)
+    err = cmd.Run()
+    if err != nil {
+        t.Execute(c.Writer, status{"设置关机任务失败"})
+    }else{
+        globalStatus.Status = "电脑将在30s后关机"
+        t.Execute(c.Writer,globalStatus )
+    }
+}
+
+func cancelShutdown(c *gin.Context){
+    args := []string{"-a"}
+    cmd := exec.Command("shutdown",args...)
+    err := cmd.Run()
+    if err != nil {
+        c.JSON(http.StatusInternalServerError,gin.H{
+            "message":fmt.Sprintf("取消关机失败,err : %v",err),
+        })
+    }else{
+        c.JSON(http.StatusOK,gin.H{
+            "message":"取消关机成功",
+        })
+    }
+}
--- remoteController/main.go
+++ remoteController/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "github.com/gin-gonic/gin"
     "html/template"
+    "io/ioutil"
     "log"
     "net"
     "net/http"
@@ -89,7 +91,7 @@ func App_Server()  {
 
 func Http_Server()  {
     e:= gin.Default()
-    e.GET("/cmd",HandleController)
+    e.POST("/cmd",HandleController)
     e.GET("/", func(context *gin.Context) {
         t,err := template.ParseFiles("index.html")
         if err != nil {
@@ -152,68 +154,119 @@ func Http_Server()  {
     
 }
 
-func main() {
-    go App_Server()
-    Http_Server()
-}
-
-func shutdown(c *gin.Context){
-    args :=[]string{"-s","-t","30"}
-    t,err := template.ParseFiles("index.html")
-    if err != nil {
-        log.Println("err : ", err)
-        return
-    }
-    cmd := exec.Command("shutdown",args...)
-    err = cmd.Run()
-    if err != nil {
-        t.Execute(c.Writer, status{"设置关机任务失败"})
-    }else{
-        globalStatus.Status = "电脑将在30s后关机"
-        t.Execute(c.Writer,globalStatus )
-    }
-}
-func shutdown2(c *gin.Context)  {
-    args :=[]string{"-s","-t","30"}
-    cmd := exec.Command("shutdown",args...)
-    err := cmd.Run()
-    if err != nil {
-       c.JSON(http.StatusInternalServerError,
-           gin.H{"message":"服务器内部错误"})
-    }else{
-        globalStatus.Status = "电脑将在30s后关机"
-        c.JSON(http.StatusOK, gin.H{"message":globalStatus.Status})
-    }
-}
-func cancelShutdown(c *gin.Context){
-    args := []string{"-a"}
-    cmd := exec.Command("shutdown",args...)
-    err := cmd.Run()
-    if err != nil {
-        c.JSON(http.StatusInternalServerError,gin.H{
-            "message":fmt.Sprintf("取消关机失败,err : %v",err),
-        })
-    }else{
-        c.JSON(http.StatusOK,gin.H{
-            "message":"取消关机成功",
-        })
-    }
+type CMDCommonRequestDTO struct {
+    CMD int `json:"CMD"`
 }
 
 func HandleController(c *gin.Context)  {
-    id := c.Query("id")
-    switch id {
-    case "1":
-        shutdown(c)
-    case "2":
-        cancelShutdown(c)
-    case "3":
-        shutdown2(c)
+    body,err := ioutil.ReadAll(c.Request.Body)
+    if err != nil {
+        ResponseError(c, 200001, "获取body失败")
+        return
+    }
+    cmdDTO := CMDCommonRequestDTO{}
+    err = json.Unmarshal(body, &cmdDTO)
+    if err != nil {
+        ResponseError(c, 200002, "解析body失败")
+        return
+    }
+    switch cmdDTO.CMD {
+    case SETSHUTDOWNPLAN_CMD:
+        setShutdownPlanHandler(c, body)
+    case CANCELSHUTDOWNPLAN_CMD:
+        cancelShutdownPlanHandler(c, body)
+    case GETMASTERVOLUME_CMD:
+        getMasterVolumeHandler(c, body)
+    case SETMASTERVOLUME_CMD:
+        setMasterVolumeHandler(c, body)
     default:
         c.JSON(
             http.StatusBadRequest,
             gin.H{
                 "message":"bad Request",
-        })
+            })
     }
 }
+func ResponseError(c *gin.Context, status int, message string){
+    c.JSON( http.StatusOK, gin.H{ "OptionStatus": status, "Message": message})
+    return
+}
+func ResponseSuccess(c *gin.Context, message string, data interface{}){
+    c.JSON(http.StatusOK, gin.H{"OptionStatus":2000000, "Message":message, "data":data})
+}
+
+
+type SetShutdownPlanRequestDTO struct {
+    ShutdownTime int `json:"ShutdownTime"`
+}
+type SetShutdownPlanResponseDTO struct {
+    ShutdownTime int `json:"ShutdownTime"`
+}
+func setShutdownPlanHandler(c *gin.Context, body []byte) {
+    var dto SetShutdownPlanRequestDTO
+    err := json.Unmarshal(body, &dto)
+    if err != nil {
+        ResponseError(c, 210001, "解析body失败")
+        return
+    }
+    retMsg, err := SetShutdownPlan(dto.ShutdownTime)
+    if err != nil {
+        ResponseError(c, 210002, fmt.Sprintf("执行计划失败,%s", err))
+        return
+    }
+    ResponseSuccess(c, retMsg, SetShutdownPlanResponseDTO{ShutdownTime: dto.ShutdownTime})
+}
+func cancelShutdownPlanHandler(c *gin.Context, body []byte) {
+    _,err := CancelShutdownPlan()
+    if err != nil {
+        ResponseError(c, 220001, fmt.Sprintf("执行失败,err:%v", err))
+        return
+    }
+    ResponseSuccess(c, "success", nil)
+    return
+}
+
+type GetMasterVolumeRequestDTO struct {
+}
+type GetMasterVolumeResponseDTO struct {
+    Volume int `json:"Volume"`
+}
+func getMasterVolumeHandler(c *gin.Context, body []byte) {
+    volume, err := GetMasterVolume()
+    if err != nil {
+        ResponseError(c, 230001, fmt.Sprintf("查询主音量失败,%s", err))
+        return
+    }
+    ResponseSuccess(c, "success", GetMasterVolumeResponseDTO{Volume: int(volume)})
+}
+
+type SetMasterVolumeRequestDTO struct {
+    Volume int `json:"Volume"`
+}
+type SetMasterVolumeResponseDTO struct {
+    Volume int `json:"Volume"`
+}
+func setMasterVolumeHandler(c *gin.Context,body []byte) {
+    requestDTO := SetMasterVolumeRequestDTO{}
+    err := json.Unmarshal(body, &requestDTO)
+    if err != nil {
+        ResponseError(c, 240001, "解析body参数失败")
+        return
+    }
+    volume, err := SetMasterVolume(float64(requestDTO.Volume))
+    if err != nil {
+        ResponseError(c, 240002, fmt.Sprintf("执行命令失败,%v", err))
+        return
+    }
+    ResponseSuccess(c, "success", SetMasterVolumeResponseDTO{Volume: int(volume)})
+}
+
+
+
+func main() {
+    // go App_Server()
+    ret, err := CancelShutdownPlan()
+    fmt.Printf("ret %s err %v\n", ret, err)
+    Http_Server()
+}
+
